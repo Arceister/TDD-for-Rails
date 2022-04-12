bf8ccfedcb666595ff2b704b0fc8a6174a7e1940
arceister <imandajagad01@gmail.com>
2022-04-13 05:22:09 +0700

    Test: `GREEN` Food is invalid with price lower than 0.01

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :price, numericality: true
+  validates :price, numericality: {greater_than: 0.01}
 
   def self.by_letter(letter)
     where("name LIKE ?", "#{letter}%").order(:name)
